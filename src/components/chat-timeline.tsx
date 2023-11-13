d15a53e5dbb8548fe3ed05f6f58ad7d6e77a9319
Hogyun Ahn <hyms26@gmail.com>
2023-11-14 03:58:26 +0900

    파일 설명

--- src/components/chat-timeline.tsx
+++ src/components/chat-timeline.tsx
@@ -1,77 +1,85 @@
-import { Firestore, Timestamp, Unsubscribe, collection, limit, onSnapshot, orderBy, query } from "firebase/firestore";
+import {
+  Firestore,
+  Timestamp,
+  Unsubscribe,
+  collection,
+  limit,
+  onSnapshot,
+  orderBy,
+  query,
+} from "firebase/firestore";
 import { useState, useEffect } from "react";
 import styled from "styled-components";
 import { db } from "../firebase";
 import Message from "./chat-message";
 
-
-//메시지 타임라인, 즉 틀 이야기하는 거 
+//메시지 타임라인, 즉 틀 이야기하는 거
 export interface IMessage {
-    id: string;
-    photo?: string;
-    message: string;
-    userId: string;
-    username: string;
-    createdAt: number;
-    time: Timestamp;
+  id: string;
+  photo?: string;
+  message: string;
+  userId: string;
+  username: string;
+  createdAt: number;
+  time: Timestamp;
 }
 
+// height = 465px -> calc(100vh - 250px) 변경 -> 반응형?
 const Wrapper = styled.div`
-    display: flex;
-    gap: 10px;
-    flex-direction: column-reverse;
-    overflow-y: auto; // 스크롤 가능하게 합니다.
-    height: 465px;
-    overflow-x: hidden;
+  display: flex;
+  gap: 10px;
+  flex-direction: column-reverse;
+  overflow-y: auto; // 스크롤 가능하게 합니다.
+  height: calc(100vh - 250px);
+  overflow-x: hidden;
 
-    &::-webkit-scrollbar {
-        display: none;
-    }
+  &::-webkit-scrollbar {
+    display: none;
+  }
 
-    /* Firefox */
-    scrollbar-width: none;
-    `;
+  /* Firefox */
+  scrollbar-width: none;
+`;
 
 export default function Timeline() {
-    const [messages, setMessage] = useState<IMessage[]>([]);
-    
-    useEffect(() => {
-        let unsubscribe: Unsubscribe | null = null;
-        const fetchMessages = async () => {
-            const messagesQuery = query(
-                collection(db, "messages"),
-                orderBy("createdAt", "desc"),
-        
-            );
+  const [messages, setMessage] = useState<IMessage[]>([]);
 
-            unsubscribe = await onSnapshot(messagesQuery, (snapshot) => {
-                const messages = snapshot.docs.map((doc) => {
-                    const { message, createdAt, userId, username, photo, time } = doc.data();
-                    return {
-                        message,
-                        createdAt,
-                        userId,
-                        username,
-                        photo,
-                        id: doc.id,
-                        time,
-                    };
-                });
-                setMessage(messages);
-            });
+  useEffect(() => {
+    let unsubscribe: Unsubscribe | null = null;
+    const fetchMessages = async () => {
+      const messagesQuery = query(
+        collection(db, "messages"),
+        orderBy("createdAt", "desc")
+      );
 
-        };
-        fetchMessages();
-        return () => {
-            unsubscribe && unsubscribe();
-        }
-    }, []);
+      unsubscribe = await onSnapshot(messagesQuery, (snapshot) => {
+        const messages = snapshot.docs.map((doc) => {
+          const { message, createdAt, userId, username, photo, time } =
+            doc.data();
+          return {
+            message,
+            createdAt,
+            userId,
+            username,
+            photo,
+            id: doc.id,
+            time,
+          };
+        });
+        setMessage(messages);
+      });
+    };
+    fetchMessages();
+    return () => {
+      unsubscribe && unsubscribe();
+    };
+  }, []);
 
-    return (
-        <Wrapper>
-            {messages.map((message) => (
-                <Message key={message.id} {...message} />
-            ))}
-        </Wrapper>
-    );
+  return (
+    <Wrapper>
+      {messages.map((message) => (
+        <Message key={message.id} {...message} />
+      ))}
+    </Wrapper>
+  );
 }
